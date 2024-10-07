d3018de4a29c4f5915bfb57fe895d1a636055608
meeran78 <meeran78@gmail.com>
2024-10-07 15:45:25 -0400

    Initial frontend template added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css';
+import reactlogo from './assets/react.svg';
+function App() {
+	const employees = [
+		{
+			id: 1,
+			name: 'John Doe',
+			title: 'Software Engineer',
+			email: 'YjJzK@example.com',
+		},
+		{
+			id: 2,
+			name: 'Jane Doe',
+			title: 'Software Engineer',
+			email: 'YjJzK@example.com',
+		},
+		{
+			id: 3,
+			name: 'Jane Doe',
+			title: 'Software Engineer',
+			email: 'YjJzK@example.com',
+		},
+		{
+			id: 4,
+			name: 'Jane Doe',
+			title: 'Software Engineer',
+			email: 'YjJzK@example.com',
+		},
+		{
+			id: 5,
+			name: 'Jane Doe',
+			title: 'Software Engineer',
+			email: 'YjJzK@example.com',
+		},
+	];
+	return (
+		<>
+ 
+		</>
+	);
+}
+
+export default App;
