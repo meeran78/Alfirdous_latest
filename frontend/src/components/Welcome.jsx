beb18354e3176f25c80358f9277062108c2820f2
meeran78 <meeran78@gmail.com>
2024-10-14 10:37:27 -0400

    Cemetry details added

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -2,39 +2,43 @@ import { useNavigate } from 'react-router-dom';
 import PageBorder from './PageBorder';
 
 const Welcome = () => {
-  const navigate = useNavigate();
-  const handleOnClick = () => {
-    navigate('/ContactUs')
-  }
-  return (
-    
-    <div className="h-full">
-      
-      <div className="flex flex-col justify-center items-center text-text-primary p-2">
-        <h1 className='text-center py-5 text-2xl '>WELCOME TO </h1>
-        <h2 className='py-3 text-xl'>ALFIRDOUS FUNERAL SERVICE</h2>
-        <div className='text-center py-3'>Muslim Funeral Burial Services from the Begining to the End including Ghusl (Religious Washing and Shrouding) and Salatul Janazah (The Funeral Prayer)</div>
-        <p className='text-center py-3 leading-10'>
-        Al Firdous funeral service, is an non-profit organization providing complete janazah services.
+	const navigate = useNavigate();
+	const handleOnClick = () => {
+		navigate('/ContactUs');
+	};
+	return (
+		<div className='h-full'>
+			<div className='flex flex-col justify-center items-center text-text-primary p-2'>
+				<h1 className='text-center py-5 text-2xl '>WELCOME TO </h1>
+				<h2 className='py-3 text-xl'>ALFIRDOUS FUNERAL SERVICE</h2>
+				<div className='text-center py-3'>
+					Muslim Funeral Burial Services from the Begining to the End including
+					Ghusl (Religious Washing and Shrouding) and Salatul Janazah (The
+					Funeral Prayer)
+				</div>
+				<p className='text-center py-3 leading-10'>
+					Al Firdous funeral service, is an non-profit organization providing
+					complete janazah services. Our mission is to provide a place for
+					preparing the janazah (body washing), performing salatul janazah
+					(funeral prayer), and burial in a dedicated Muslim cemetery. Our
+					vision is to keep these services low-cost for everyone and free for
+					those who cannot afford such expenses at their time of grief and pain.
+					The body washing facility is located within Masjid Yousuf (ICR),
+					Richomond, Virginia and the cemetery grounds are located within an hr
+					drive from the Masjid. These facilities is ideal for providing
+					services to Muslims in the Richmond, Chesterfield and surrounding
+					areas.
+				</p>
+				<div>
+					<button onClick={handleOnClick} className='mb-10'>
+						Contact Us
+					</button>
+				</div>
+			</div>
+			{/* <hr className="w-[380px] h-[1px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10"></hr> */}
+			<PageBorder />
+		</div>
+	);
+};
 
-Our mission is to provide a place for preparing the janazah (body washing), performing salatul janazah (funeral prayer), and burial in a dedicated Muslim cemetery.
-
-Our vision is to keep these services low-cost for everyone and free for those who cannot afford such expenses at their time of grief and pain.
-
-The body washing facility is located within Masjid Yousuf (ICR), Richomond, Virginia and the cemetery grounds are located within an hr drive from the Masjid.
-
-These facilities is ideal for providing services to Muslims in the Richmond, Chesterfield and surrounding areas.
-        </p>
-        <div>
-          <button onClick={handleOnClick} className='mb-10' >Contact Us</button>
-        </div>
-       
-      </div>
-      {/* <hr className="w-[380px] h-[1px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10"></hr> */}
-      <PageBorder />
-    </div>
-    
-  )
-}
-
-export default Welcome
+export default Welcome;
--- frontend/src/screens/Cemeteries/index.jsx
+++ frontend/src/screens/Cemeteries/index.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React from 'react';
+
+const cemeteryList = [
+	{
+		id: 1,
+		name: 'Masjid Yousuf',
+		address: [
+			{
+				city: 'Richmond',
+				state: 'VA',
+				country: 'USA',
+				postalCode: '23228',
+				street: '123 Main St',
+			},
+		],
+		phone: '540-777-8888',
+		email: '4CvZM@example.com',
+		website: 'https://www.masjidyousuf.com/',
+		lat: 37.540777,
+		lng: -77.436777,
+	},
+	{
+		id: 2,
+		name: 'Masjid Al-Husain',
+		address: [
+			{
+				city: 'Richmond',
+				state: 'VA',
+				country: 'USA',
+				postalCode: '23228',
+				street: '123 Main St',
+			},
+		],
+		phone: '540-777-8888',
+		email: '4CvZM@example.com',
+		website: 'https://www.masjidyousuf.com/',
+		lat: 37.540777,
+		lng: -77.436777,
+	},
+	{
+		id: 3,
+		name: 'Masjid Al-Husain',
+		address: [
+			{
+				city: 'Richmond',
+				state: 'VA',
+				country: 'USA',
+				postalCode: '23228',
+				street: '123 Main St',
+			},
+		],
+		phone: '540-777-8888',
+		email: '4CvZM@example.com',
+		website: 'https://www.masjidyousuf.com/',
+		lat: 37.540777,
+		lng: -77.436777,
+	},
+];
 
 const Cemeteris = () => {
-  return (
-    <div>Cemeteris</div>
-  )
-}
+	return (
+		<div className='text-text-primary'>
+			<h1 className='text-3xl text-center p-5 '>Muslim Cemeteries</h1>
+			<div className='grid grid-cols-3 gap-10 p-10 text-xl'>
+				{cemeteryList.map((cemetery) => (
+					<div key={cemetery.id}>
+						<h2 className='text-2xl'>{cemetery.name}</h2>
+						<div className=''>
+							{cemetery.address.map((address) => (
+								<div key={address.id} className='flex flex-col gap-2 mx-4'>
+									<div>{address.street}</div>
+									<div>{address.city}</div>
+									<div>{address.state}</div>
+									<div>{address.country}</div>
+									<div>{address.postalCode}</div>
+								</div>
+							))}
+						</div>
+						<p>{cemetery.phone}</p>
+						<p>{cemetery.email}</p>
+						<p>{cemetery.website}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
 
-export default Cemeteris
+export default Cemeteris;
